f824df0b75230c4e4c1ec9153d0203846e436280
yaz_1710 <yahyahosny1234567@gmail.com>
2026-08-29 18:29:02 +0300

    1st answer

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Book
+{
+    public string title;
+    public int pages;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Book book = new Book();
+        book.title = "Clean Code";
+        book.pages = 464;
+
+        object obj = book;
+        Console.WriteLine(obj);
+    }
+}
+//1st answer
